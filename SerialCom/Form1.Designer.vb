1994fb9ea8333a693d255f1db1f1a5aef9c7a023
electro soft projects solutions <yourstm12@gmail.com>
2020-07-15 21:28:46 +0530

    inital commit

---
+++ SerialCom/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea9 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend17 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Legend18 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.chart = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        CType(Me.chart, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button1.Location = New System.Drawing.Point(1224, 130)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 48)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "CONN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1224, 382)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(140, 48)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "START"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(1224, 29)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(140, 33)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.Color.White
+        Me.RichTextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox2.HideSelection = False
+        Me.RichTextBox2.Location = New System.Drawing.Point(13, 12)
+        Me.RichTextBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(210, 479)
+        Me.RichTextBox2.TabIndex = 5
+        Me.RichTextBox2.Text = ""
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button4.Location = New System.Drawing.Point(1224, 197)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(140, 42)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "DISCONN"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM6"
+        Me.SerialPort1.ReadBufferSize = 2048
+        Me.SerialPort1.ReceivedBytesThreshold = 40
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"9600", "38400", "57600", "115200"})
+        Me.ComboBox2.Location = New System.Drawing.Point(1224, 80)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(140, 33)
+        Me.ComboBox2.TabIndex = 9
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(1224, 437)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(140, 43)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "STOP"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'chart
+        '
+        ChartArea9.AlignmentOrientation = CType((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical Or System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal), System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)
+        ChartArea9.AxisY.Maximum = 60.0R
+        ChartArea9.AxisY.Minimum = 0R
+        ChartArea9.AxisY2.Maximum = 60.0R
+        ChartArea9.AxisY2.Minimum = 0R
+        ChartArea9.Name = "ChartArea1"
+        Me.chart.ChartAreas.Add(ChartArea9)
+        Legend17.Name = "eeg1"
+        Legend18.Name = "eeg2"
+        Me.chart.Legends.Add(Legend17)
+        Me.chart.Legends.Add(Legend18)
+        Me.chart.Location = New System.Drawing.Point(227, 9)
+        Me.chart.Margin = New System.Windows.Forms.Padding(0)
+        Me.chart.Name = "chart"
+        Me.chart.Size = New System.Drawing.Size(955, 574)
+        Me.chart.TabIndex = 31
+        Me.chart.Text = "Chart1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(1224, 544)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(140, 42)
+        Me.Button5.TabIndex = 32
+        Me.Button5.Text = "save"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"5", "10", "15", "continue"})
+        Me.ComboBox3.Location = New System.Drawing.Point(1224, 256)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(140, 33)
+        Me.ComboBox3.TabIndex = 33
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(1227, 495)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(127, 31)
+        Me.RichTextBox1.TabIndex = 34
+        Me.RichTextBox1.Text = ""
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(1239, 305)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(80, 30)
+        Me.RadioButton1.TabIndex = 35
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "10bit"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(1239, 341)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(80, 30)
+        Me.RadioButton2.TabIndex = 36
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "12bit"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(1366, 595)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.chart)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.chart, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents chart As DataVisualization.Charting.Chart
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ SerialCom/Form1.vb
@@ -0,0 +1,242 @@
+﻿Imports System
+Imports System.Threading
+Imports System.IO.Ports
+Imports System.ComponentModel
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class Form1
+    '------------------------------------------------
+    Private elapsedTime As Integer
+    Dim myPort As Array
+    Dim val1 As Double
+    Dim rtxt As String
+    Dim trac1 As Double
+    Dim countt As Double
+    Dim refch0 As Double
+    Dim refch1 As Double
+    Dim tmp1 As Double
+    Dim tmp2 As Double
+    ' Dim strWords As String
+    '  Dim part As String
+    Dim i As Integer
+    Dim sidex As Integer
+
+    Delegate Sub SetTextCallback(ByVal [text] As String) 'Added to prevent threading errors during receiveing of data
+    '------------------------------------------------
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        ComboBox1.Items.AddRange(myPort)
+        RadioButton2.Checked = True
+
+
+        Button2.Enabled = False
+        RichTextBox2.Focus()
+        chart.Series.Clear()
+        rtxt = ""
+        trac1 = 0
+        sidex = 1
+        chart.Series.Add("eeg1")
+        chart.Series(0).ChartType = SeriesChartType.FastLine
+        chart.Series.Add("eeg2")
+        chart.Series(1).ChartType = SeriesChartType.FastLine
+        ' chart.Series.Add("eeg3")
+        ' chart.Series(2).ChartType = SeriesChartType.FastLine
+        ' chart.Titles.Clear()
+        chart.Titles.Add(New Title("eeg Graph..", Docking.Top, New Font(Font, FontStyle.Bold), Color.Black))
+        chart.ChartAreas(0).AxisY.IsStartedFromZero = 0
+        chart.ChartAreas(0).AxisY.Maximum = Double.NaN
+        chart.ChartAreas(0).AxisY.Minimum = Double.NaN
+        RichTextBox2.Visible = True 'False
+        chart.ChartAreas(0).AxisX.MajorGrid.LineWidth = 0
+        chart.ChartAreas(0).AxisY.MajorGrid.LineWidth = 0
+        refch0 = 0
+        refch1 = 4100
+
+
+        If ComboBox1.SelectedIndex = -1 Then
+            ComboBox1.SelectedIndex = 0
+        End If
+
+        If ComboBox2.SelectedIndex = -1 Then
+            ComboBox2.SelectedIndex = 3
+        End If
+
+        If ComboBox3.SelectedIndex = -1 Then
+            ComboBox3.SelectedIndex = 0
+        End If
+
+
+    End Sub
+    '------------------------------------------------
+
+    '------------------------------------------------
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        SerialPort1.PortName = ComboBox1.Text  ' con button
+        SerialPort1.BaudRate = ComboBox2.Text
+        '  SerialPort1.StopBits = StopBits.None
+        ' SerialPort1.ReadBufferSize = AutoSize
+
+        SerialPort1.Parity = Parity.None
+        SerialPort1.Encoding = Encoding.ASCII
+        SerialPort1.Open()
+        Button1.Enabled = False
+        Button2.Enabled = True
+        Button4.Enabled = True
+
+    End Sub
+    '------------------------------------------------
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+
+        SerialPort1.Write("t") 'concatenate with \n  start button
+        chart.Series(0).Points.Clear()
+        chart.Series(1).Points.Clear()
+        'chart.Series(2).Points.Clear()
+        Timer1.Start()
+        elapsedTime = 0
+        countt = 0
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Timer1.Stop()
+        SerialPort1.Close()
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Button4.Enabled = False
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As System.Object, e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ReceivedText(SerialPort1.ReadExisting())
+    End Sub
+
+    Private Sub ReceivedText(ByVal [text] As String) 'input from ReadExisting
+
+        If Me.RichTextBox2.InvokeRequired Then
+            Dim x As New SetTextCallback(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+        Else
+
+
+            Me.RichTextBox2.Text &= [text] 'append text
+            ' Me.RichTextBox2.Text = Me.RichTextBox2.Text.Insert(0, text)
+
+            Me.RichTextBox2.Select(RichTextBox2.Text.Length, 0)
+            Me.RichTextBox2.ScrollToCaret()
+
+
+            rtxt &= [text]
+
+            If rtxt.Contains(vbLf) Then
+
+
+                ' Dim part As String() = rtxt.Split(New String() {Environment.NewLine},
+                ' StringSplitOptions.None)
+
+                i = rtxt.IndexOf(vbLf)
+                Dim f As String = rtxt.Substring(i + 1, rtxt.IndexOf(vbLf, i + 1) - i - 1)
+                '  sidex = i + 1 + sidex
+
+
+
+                Dim strWords As String() = f.Split(" ")
+
+
+                trac1 = trac1 + 1
+                'And (Math.Abs(CInt(strWords(0)) - tmp1) < 1000)
+                If IsInteger(strWords(0)) Then
+                    chart.Series(0).Points.AddXY(elapsedTime, CInt(strWords(0)) + refch0)
+                    tmp1 = CInt(strWords(0))
+                Else
+                    chart.Series(0).Points.AddXY(elapsedTime, tmp1 + refch0)
+                End If
+                'And (Math.Abs(CInt(strWords(1)) - tmp2) < 1000)
+                If IsInteger(strWords(1)) Then
+                    chart.Series(1).Points.AddXY(elapsedTime, CInt(strWords(1)) + refch1)
+                    tmp2 = CInt(strWords(1))
+                Else
+                    chart.Series(1).Points.AddXY(elapsedTime, tmp2 + refch1)
+                End If
+
+                elapsedTime = elapsedTime + 1
+                'text = ""
+                rtxt = ""
+            End If
+        End If
+        'chart.Series(2).Points.AddXY(elapsedTime, CInt(strWords(2)) + 2000)
+        'PeltierCellValue = PeltierCellValue + 2
+
+
+
+    End Sub
+
+    Public Function IsInteger(value As Object) As Boolean
+        Dim output As Integer ' I am not using this by intent it is needed by the TryParse Method
+        If (Integer.TryParse(value.ToString(), output)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
+    Private Sub RichTextBox2_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox2.TextChanged
+        RichTextBox2.ScrollToCaret()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SerialPort1.Write("s") 'concatenate with \n
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'SerialPort1.Write("s") 'concatenate with \n
+        countt = countt + 1
+        RichTextBox1.Text = countt.ToString
+
+        If ComboBox3.SelectedIndex < 3 Then
+
+            If countt >= CInt(ComboBox3.Text) Then
+
+                Timer1.Stop()
+                SerialPort1.Write("s")
+                MsgBox("Log time reach ! " & ControlChars.NewLine & " You can save it ")
+
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+
+        Dim sfd As New SaveFileDialog() ' this creates an instance of the SaveFileDialog called "sfd"
+        sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        sfd.FilterIndex = 1
+        sfd.RestoreDirectory = True
+        If sfd.ShowDialog() = DialogResult.OK Then
+            Dim FileName As String = sfd.FileName ' retrieve the full path to the file selected by the user
+            Dim sw As New System.IO.StreamWriter(FileName, False) ' create a StreamWriter with the FileName selected by the User
+            sw.WriteLine(RichTextBox2.Text) ' Write the contents of TextBox to the file
+            sw.Close() ' close the file
+        End If
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        refch0 = 0
+        refch1 = 1100
+        RadioButton2.Checked = False
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        refch0 = 0
+        refch1 = 4100
+        RadioButton1.Checked = False
+    End Sub
+
+End Class
